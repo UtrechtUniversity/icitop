a165ae6cc33909a8991fc8e6745e7a8f96acfe15
DorienHuijser <dorienhuijser@outlook.com>
2023-12-22 11:29:07 +0100

    rewrite new_column function to be simpler to use

--- R/20231208-add-values.R
+++ R/20231208-add-values.R
@@ -1,8 +1,11 @@
 # add columns
-
-new_column<-function(dataset, new_name, input){
-  for (i in seq_along(new_name)){
-    dataset[[new_name[i]]] <- input[i]
+new_column <- function(dataset, new_columns_df) {
+  
+  # For every column in new_columns_df
+  for(col in seq_along(colnames(new_columns_df))){
+    
+    # Create that same column in the dataset and assign it the values from new_columns_df
+    dataset[[colnames(new_columns_df[col])]] <- new_columns_df[1, col]
   }
   return(dataset)
-} 
+  }
