d7e6364f7cc312644f9e0f3c8bfbf7f020e7bd76
Geeraerts <s.b.geeraerts@uu.nl>
2023-12-22 19:41:07 +0100

    made a seperate file for the first step of datacleaning - and applied this to a different dataset, which included making a large file out of many files

---
+++ R/make_lowercase.R
@@ -0,0 +1,14 @@
+#' Function to convert column names  to lowercase.
+#'  
+#' @param df The dataframe/tibble in which columnnames are converted to lowercase
+#' @returns The dataframe/tibble with renamed columns.
+#' @examples
+#' lower_case_mtcars <- data.frame(name = c("mpg", "cyl"),
+#'                           new_label = c("miles per gallon","cylinder size"))
+#' lowercase <- convert_to_lowercase(lower_case_mtcars)
+
+convert_to_lowercase <- function(df) {
+  # Convert column names to lowercase
+  colnames(df) <- tolower(colnames(df))
+  return(df)
+}
