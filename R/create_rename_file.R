f87787147e8c7df1745ab520b7e8873cc13ac2e6
Geeraerts <s.b.geeraerts@uu.nl>
2024-01-20 00:45:45 +0100

    added a new column to rename file to change category labels

--- R/create_rename_file.R
+++ R/create_rename_file.R
@@ -4,7 +4,7 @@
 #' @param codebook A codebook dataframe with columns "name" and "label" as the first 2 columns.
 #' @param study The name of the study which should be saved in the file name, e.g., "102.us-lls"
 #' @param path A path to the folder where the rename files should be written, e.g., "data/102.us-lls/2.data-checks".
-#' @returns An empty rename object, with columns for name, label, generation, wave, etc.
+#' @returns An empty rename object, with columns for name, label, datatype, value labels, generation, wave, etc.
 #' @examples
 #' rename.empty <- create_rename_file(data.dictionary, "102.us-lls", "data/102.us-lls/2.data-checks")
 # Dependencies
@@ -30,8 +30,10 @@ create_rename_file <- function(codebook, study, path){
       name_construct = NA,
       new_name = NA,
       new_label = NA,
+      new_value_label=NA,
       comments = NA
     )
+  
   
   # Write the first 2 columns of the codebook to csv
   # Save the empty version
@@ -39,11 +41,11 @@ create_rename_file <- function(codebook, study, path){
             paste0(path, "/1_rename_", study, "_empty.csv"),
             row.names = FALSE)
   
-  # And save the to be filled version if it's not already there
+  # And save the to be filled version if it's not already there into excel
   to_be_filled_file <- paste0(path, "/1_rename_", study, "_filled.xlsx")
   
   if (!file.exists(to_be_filled_file)) {
-    # Write the CSV file only if it doesn't exist
+    # Write the excel file only if it doesn't exist
     openxlsx::write.xlsx(renameobject, to_be_filled_file,rowNames = FALSE)
     cat("Excel file written:", to_be_filled_file, "\n")
   } else {
