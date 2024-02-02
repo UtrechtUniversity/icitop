990ac6a46faa5b8d6c2ca2b6a58a726fbcfee81b
DorienHuijser <dorienhuijser@outlook.com>
2024-02-02 17:18:20 +0100

    fix example

--- R/same_id.R
+++ R/same_id.R
@@ -1,11 +1,11 @@
 #' Determines whether all dataframes in a list have the same specified id variable.
 #' If not all dataframes have the specified id variable, provides a list of dataframes without specified id variable
 #' 
-#' @param dfs The list with dataframes.
+#' @param dfs A list with dataframes. Each list item should have be named.
 #' @param id the name of the id variable.
 #' @returns a statement whether id variable is present. If not all dataframes have the  id variable: a list of dataframes without id variable
 #' @examples
-#' dfs<-list(mtcars, iris)
+#' dfs <- list(mtcars = mtcars, iris = iris)
 #' id_check(dfs, "mpg") # All dataframes have the specified variable.
 #' id_check(dfs, "id")
 
@@ -17,10 +17,9 @@ id_check <- function(dfs, id) {
   if (length(dataframes_without_id) == 0) {
     cat("All dataframes have the specified variable.\n")
   } else {
-    cat("Dataframes without specified id variable: ", paste(dataframes_without_id, collapse = ", "), "\n")
+    cat("Dataframes without specified id variable: ", paste(dataframes_without_id, collapse = "\n\n"), "\n")
   }
   
   # Return the result
   return(dataframes_without_id)
 }
-
