97c15ec2d8b016c4d75ef2706f8b567ef87e767a
Dorien Huijser <dorienhuijser@outlook.com>
2024-02-06 17:40:54 +0100

    add some g2 rows to rename_basic, put code in function

--- R/check_data_presence.R
+++ R/check_data_presence.R
@@ -2,23 +2,17 @@
 # For all waves in which parenting variables are measured, 
 # is the moderator  also present? 
 # result is a dataframe with the waves for each variable
- 
-## START New edits Dorien on 29th Jan 2024
-# With thanks to ChatGPT :)
-# Prompt and full answer can be seen here: https://chat.openai.com/share/abdd4db2-284d-442c-af56-93ace2e177fa
 
-# Note Dorien: As most of this code has to be repeated 3 times (g1, g2, g3), 
-# we could create a function instead and apply it on all 3 generations?
-
-# For testing, add some wave and construct info to rename_basic (remove later)
-rename_basic$wave[84:88] <- c("w1", "w2", "v1", "v2", "v2")
-rename_basic$name_construct[84:88] <- c("par", "par", "par", "par", "edu")
-rename_basic$generation[84:88] <- c("g1", "g1", "g2", "g2", "g2")
+### For testing, add some wave and construct info to rename_basic (remove later) ####
+rename_basic$wave[84:90] <- c("w1", "w2", "v1", "v2", "v2", "w1", "w2")
+rename_basic$name_construct[84:90] <- c("par", "par", "par", "par", "edu", "par", "occ")
+rename_basic$generation[84:90] <- c("g1", "g1", "g2", "g2", "g2", "g2", "g2")
 rename_basic$target[84:86] <- c("f", "m", "c2")
 
-# Filter dataframe for constructs of interest G1
+### Filter dataframe for constructs of interest G1 ####
 # I made an excel sheet with information about (a selection of) the moderators and par 
-# I made a seperate one for G1 par and G2 par, but script below is only about g1 par
+# I made a separate one for G1 par and G2 par, but script below is only about g1 par
+# Note Dorien: minor detail, you may consider converting the excel to csv later (csv = open format, xlsx = closed format)
 
 constructs_of_interest_g1 <-read_xlsx("docs/g1-moderators.xlsx")
 
@@ -27,42 +21,64 @@ constructs_of_interest_g1 <-read_xlsx("docs/g1-moderators.xlsx")
 # (I now ignored target; not sure yet if I need it)
 # rows should only be selected if they meet ALL criteria
 
-relevant_data_g1 <- inner_join(constructs_of_interest_g1, rename_basic, by = c("generation", "name_construct"))
+relevant_data_g1 <- inner_join(constructs_of_interest_g1, 
+                               rename_basic, 
+                               by = c("generation", "name_construct"))
 
 # this gives a warning, but it does seem to do what I want
 # the variable in cell 88 (g2 edu - which is only relevant for G2 par) is ignored, but g1 edu (which IS relevant for g1 par) is included. 
 
-# Create a tabular overview of waves per construct > This actually creates a list. The rest below hasn't been changed
-waves_per_construct_g1 <- tapply(relevant_data_g1$wave, 
-                                 relevant_data_g1$name_construct, 
-                                 function(x) unique(x))
+# Testing for g2: ####
+constructs_of_interest_g2 <-read_xlsx("docs/g2-moderators.xlsx")
+relevant_data_g2 <- inner_join(constructs_of_interest_g2, 
+                               rename_basic, 
+                               by = c("generation", "name_construct"))
 
-par_wave_g1 <- waves_per_construct_g1[["par"]]
-
-missing_variables_df <- data.frame(construct = character(), 
-                                   missing_waves = character(), 
-                                   stringsAsFactors = FALSE)
-
-for (construct in names(waves_per_construct_g1)) {
-  if (construct != "par") {
-    other_construct_wave <- waves_per_construct_g1[[construct]]
-    missing_variables <- setdiff(par_wave_g1, other_construct_wave)
-    
-    if (length(missing_variables) > 0) {
-      # Print results to console
-      cat("Variables not present in the same wave as 'par' for construct", construct, ":", paste(missing_variables, collapse = ", "), "\n")
+# Suggested function ####
+# Note Dorien: can we create a function for the rows from hereon? Or is that too 
+# complicated because of the different requirements per generation?
+# for example (does not work for g2 right now because no par in g2 yet):
+detect_missing_waves <- function(relevant_data_df){
+  waves_per_construct <- tapply(relevant_data_df$wave, 
+                                relevant_data_df$name_construct, 
+                                function(x) unique(x))
+  
+  par_wave <- waves_per_construct[["par"]]
+  
+  missing_waves_df <- data.frame(construct = character(), 
+                                 missing_waves = character(), 
+                                 stringsAsFactors = FALSE)
+  
+  for (construct in names(waves_per_construct)) {
+    if (construct != "par") {
+      other_construct_wave <- waves_per_construct[[construct]]
+      missing_waves <- setdiff(par_wave, other_construct_wave)
       
-      # And save the results in the dataframe
-      missing_variables_df <- rbind(missing_variables_df, data.frame(construct = construct, missing_waves = paste(missing_variables, collapse = ", ")))
+      if (length(missing_waves) > 0) {
+        # Print results to console
+        cat("Variables not present in the same wave as 'par' for construct", 
+            construct, ":", 
+            paste(missing_waves, 
+                  collapse = ", "), 
+            "\n")
+        
+        # And save the results in the dataframe
+        missing_waves_df <- rbind(missing_waves_df, 
+                                      data.frame(construct = construct, 
+                                                 missing_waves = paste(missing_waves, 
+                                                                       collapse = ", ")))
+      }
     }
   }
-} 
+  return(missing_waves_df)
+}
 
 
 
-#### OLD
+#### OLD ####
 
-
+# With thanks to ChatGPT :)
+# Prompt and full answer can be seen here: https://chat.openai.com/share/abdd4db2-284d-442c-af56-93ace2e177fa
 
 # Filter dataframe for constructs of interest G2
 constructs_of_interest_g2 <- c("eth", 
@@ -151,4 +167,3 @@ for (construct in names(waves_per_construct_g1)) {
 }
 
 # Now the missing_variables_df can be saved for later consultation
-
